77ea1e1a0a5ed33263a362f94548ce91e9a6b6a5
Werlei M Andrade <werlleymendes@hotmail.com>
2025-08-06 08:57:17 -0300

    Conclusão v 1.0

--- Relatório itens ativos mês.sql
+++ Relatório itens ativos mês.sql
@@ -2,12 +2,49 @@ SELECT COUNT(DISTINCT a.seqproduto) "ITENS CADASTRADOS",
        (SELECT COUNT (DISTINCT b.seqproduto)
                FROM consinco.mrl_produtoempresa b
                where b.nroempresa = 1 AND
-                     b.estqloja > 0) "ITENS EM ESTOQUE" ,
-        (SELECT COUNT (DISTINCT c.seqproduto)
-               FROM consinco.mrl_produtoempresa c
-               where c.nroempresa = 1 AND
-                     TRUNC(c.dtaultvenda) >= '01-jul-2025' ) "ITENS VENDIDOS"        
-       FROM consinco.mrl_prodempseg a
+                     b.estqloja > 0) "ITENS EM ESTOQUE" ,                     
+        (SELECT COUNT(DISTINCT c.SEQPRODUTO)
+                FROM consinco.MAXV_ABCDISTRIBBASE c
+                WHERE c.dtavda BETWEEN '01/jul/2025' and '31/jul/2025'
+                and c.nroempresa = 1) "ITENS VENDIDOS",
+        ROUND(((SELECT COUNT(DISTINCT c.SEQPRODUTO)
+                FROM consinco.MAXV_ABCDISTRIBBASE c
+                WHERE c.dtavda BETWEEN '01/jul/2025' and '31/jul/2025'
+                and c.nroempresa = 1) / (SELECT COUNT (DISTINCT b.seqproduto)
+               FROM consinco.mrl_produtoempresa b
+               where b.nroempresa = 1 AND
+                     b.estqloja > 0 )* 100),2) "% ITENS VENDIDOS EM RELACAO AO ESTOQUE",
+        (COUNT(DISTINCT A.SEQPRODUTO) -
+                     (SELECT COUNT (DISTINCT b.seqproduto)
+                             FROM consinco.mrl_produtoempresa b
+                             where b.nroempresa = 1 AND
+                             b.estqloja > 0)) "ITENS CADASTRADOS SEM ESTOQUE",
+         ROUND(((COUNT(DISTINCT A.SEQPRODUTO) -
+                     (SELECT COUNT (DISTINCT b.seqproduto)
+                             FROM consinco.mrl_produtoempresa b
+                             where b.nroempresa = 1 AND
+                             b.estqloja > 0)) /  COUNT(DISTINCT A.SEQPRODUTO)) * 100, 2)                             
+                             "% ITENS INEXISTENTES EM RELACAO AO CADSTRO",
+         (SELECT COUNT (DISTINCT b.seqproduto)
+               FROM consinco.mrl_produtoempresa b
+               where b.nroempresa = 1 AND
+                     b.estqloja > 0) -  
+                     (SELECT COUNT(DISTINCT c.SEQPRODUTO)
+                FROM consinco.MAXV_ABCDISTRIBBASE c
+                WHERE c.dtavda BETWEEN '01/jul/2025' and '31/jul/2025'
+                and c.nroempresa = 1) "ITENS EM ESTOQUE SEM VENDA NO MES",
+           ROUND(((((SELECT COUNT (DISTINCT b.seqproduto)
+               FROM consinco.mrl_produtoempresa b
+               where b.nroempresa = 1 AND
+                     b.estqloja > 0) -  
+                     (SELECT COUNT(DISTINCT c.SEQPRODUTO)
+                FROM consinco.MAXV_ABCDISTRIBBASE c
+                WHERE c.dtavda BETWEEN '01/jul/2025' and '31/jul/2025'
+                and c.nroempresa = 1))/(SELECT COUNT (DISTINCT b.seqproduto)
+               FROM consinco.mrl_produtoempresa b
+               where b.nroempresa = 1 AND
+                     b.estqloja > 0))*100),2) "% ITENS EM ESTOQUE SEM VENDA NO MES"
+           FROM consinco.mrl_prodempseg a
 WHERE a.nroempresa = 1 AND
       a.statusvenda = 'A' AND
       a.nrosegmento = 1
@@ -18,7 +55,10 @@ ORDER BY 1;
 
 
 
-SELECT * FROM consinco.mrl_produtoempresa a
+SELECT * FROM consinco.mrl_produtoempresa a;
 
-SELECT * FROM consinco.MAXV_ABCDISTRIBBASE a
+SELECT COUNT(DISTINCT A.SEQPRODUTO)
+       FROM consinco.MAXV_ABCDISTRIBBASE a
+       WHERE a.dtavda BETWEEN '01/jul/2025' and '31/jul/2025'
+             and a.nroempresa = 1;
 
